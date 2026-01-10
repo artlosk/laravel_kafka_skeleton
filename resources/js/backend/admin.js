45e51ca33ee3148166ef2e87a11c092230c159f1
loskutnikoff <artlosk@gmail.com>
2026-01-10 13:55:46 +0600

    fix-media-gallery
    (cherry picked from commit e1f68b0a20bd4c90622d0659b446e779178970c6)

--- resources/js/backend/admin.js
+++ resources/js/backend/admin.js
@@ -1,11 +1,20 @@
 import '../../js/bootstrap';
-import 'bootstrap';
 import 'admin-lte';
+import toastr from 'toastr';
+import 'toastr/build/toastr.css';
 
 import './darkMode';
 import {initDateInputs} from './dateInput';
 import './users';
 import './post-notification-settings';
+
+window.toastr = toastr;
+toastr.options = {
+    closeButton: true,
+    progressBar: true,
+    positionClass: 'toast-top-right',
+    timeOut: '5000'
+};
 
 initDateInputs();
 
